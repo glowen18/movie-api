aa86d64763e1ad3d2831830b9f6de7c186a1f372
Gaye Lowenstein <glowen@hotmail.com>
2017-01-13 10:09:35 -0800

    created results.ejs to view movie titles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 app.set('port', process.env.PORT || 3000);
 
@@ -9,8 +10,9 @@ app.get('/results', function(req, res){
   request("http://www.omdbapi.com/?s=california", function(error, response, body){
     if(!error && response.statusCode == 200){
     //turn string into object to access key|value pairs
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]["Title"]);
+      var data = JSON.parse(body);
+      // res.send(results["Search"][0]["Title"]);
+      res.render("results", {data: data});
     }
   });
 });
